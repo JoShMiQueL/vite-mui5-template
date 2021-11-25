e19eb00b6e6f4929e99a21b21b641feac5c611a8
JoShMiQueL <joshmiquel.cs@gmail.com>
2021-11-26 00:57:57 +0100

    First commit

---
+++ src/views/Home/index.jsx
@@ -0,0 +1,11 @@
+export default function Home(props) {
+  return (
+    <div {...props}>
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos,
+        voluptatum.
+      </p>
+    </div>
+  );
+}
